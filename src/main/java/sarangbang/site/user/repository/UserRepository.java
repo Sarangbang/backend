35ee68bfec60d6beb659fb98ae9d6a8e127fa2e2
apfp77 <apfp77@naver.com>
2025-06-15 23:19:45 +0900

    feat: 회원가입 기능 추가

--- src/main/java/sarangbang/site/user/repository/UserRepository.java
+++ src/main/java/sarangbang/site/user/repository/UserRepository.java
@@ -3,8 +3,8 @@ package sarangbang.site.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sarangbang.site.user.entity.User;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    List<User> findAllByJob(String job);
+    Optional<User> findByEmail(String email);
 }
