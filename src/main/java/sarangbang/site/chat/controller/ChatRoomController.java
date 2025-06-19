0575adf462c7dc2029445a914491e21002422486
bo <bo9718@naver.com>
2025-06-19 21:38:05 +0900

    refactor: 인증 사용자 정보 연동

--- src/main/java/sarangbang/site/chat/controller/ChatRoomController.java
+++ src/main/java/sarangbang/site/chat/controller/ChatRoomController.java
@@ -2,11 +2,14 @@ package sarangbang.site.chat.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sarangbang.site.chat.dto.ChatRoomCreateRequestDto;
 import sarangbang.site.chat.dto.ChatRoomSummaryResponseDto;
 import sarangbang.site.chat.dto.UserChatRoomSummaryDto;
 import sarangbang.site.chat.service.ChatRoomService;
+import sarangbang.site.security.details.CustomUserDetails;
+
 import java.util.List;
 
 @RestController
@@ -16,18 +19,22 @@ public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
 
+    /* 임시 로그인 인증 */
+    @GetMapping("/api/test/test2")
+    public void test(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        System.out.println(userDetails.getId());
+    }
+
     @PostMapping
-    public ResponseEntity<ChatRoomSummaryResponseDto> createRoom(@RequestBody ChatRoomCreateRequestDto request) {
-        String userId = "UUID1";
-        ChatRoomSummaryResponseDto responseDto = chatRoomService.createRoom(request, userId);
+    public ResponseEntity<ChatRoomSummaryResponseDto> createRoom(@RequestBody ChatRoomCreateRequestDto request, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        ChatRoomSummaryResponseDto responseDto = chatRoomService.createRoom(request, userDetails.getId());
         ResponseEntity<ChatRoomSummaryResponseDto> response = ResponseEntity.ok(responseDto);
         return response;
     }
 
     @GetMapping
-    public ResponseEntity<List<UserChatRoomSummaryDto>> getAllRooms() {
-        String userId = "UUID1";
-        List<UserChatRoomSummaryDto> responseDto = chatRoomService.getAllRooms(userId);
+    public ResponseEntity<List<UserChatRoomSummaryDto>> getAllRooms(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<UserChatRoomSummaryDto> responseDto = chatRoomService.getAllRooms(userDetails.getId());
         ResponseEntity<List<UserChatRoomSummaryDto>> response = ResponseEntity.ok(responseDto);
         return response;
     }
