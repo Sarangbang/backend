9aa5a57f101dc907e359e02b717c84378fa8f1d8
kimyoungjun-dev <kdaon1130@gmail.com>
2025-06-19 20:03:22 +0900

    로그인 기능 추가 (#11)
    * feat: jwt 라이브러리 추가
    
    * feat: jwt 로그인 기능 추가
    
    * refactor: 사용하지않는 import 제거 및 파일 제거
    
    * feat: test user nickname 추가
    
    * feat: CostomuserDetails 추가, 커스텀핸들러 추가
    
    * refactor: 불필요한 코드 및 파일 삭제
    
    * feat: 로깅 추가, user service에 getUserById 메서드 추가
    
    * feat: UsernamePasswordAuthenticationToken 안에 CustomUserDetails 추가
    
    * feat: user entity UUID 자동생성 -> 수동생성으로 변경
    
    ---------
    
    Co-authored-by: apfp77 <apfp77@naver.com>

---
+++ src/main/java/sarangbang/site/auth/controller/AuthController.java
@@ -0,0 +1,42 @@
+package sarangbang.site.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.*;
+import sarangbang.site.auth.dto.SignInRequest;
+import sarangbang.site.security.details.CustomUserDetails;
+import sarangbang.site.security.jwt.JwtTokenProvider;
+
+@RestController
+@RequestMapping("/api/users")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<Void> login(@RequestBody SignInRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+
+        String token = jwtTokenProvider.createToken(
+                user.getId(),           // UUID
+                user.getEmail(),        // email
+                user.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .toList()
+        );
+
+        return ResponseEntity.ok().header("Authorization", "Bearer " + token).build();
+    }
+}
