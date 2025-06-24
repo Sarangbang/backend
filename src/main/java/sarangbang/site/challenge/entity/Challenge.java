b69abc031e7082a3d8e36b13968c626d5cdffb3f
SeongriPark <tjdfl0516@gmail.com>
2025-06-24 15:00:19 +0900

    엔티티, 레포 등록

--- src/main/java/sarangbang/site/challenge/entity/Challenge.java
+++ src/main/java/sarangbang/site/challenge/entity/Challenge.java
@@ -1,20 +1,24 @@
 package sarangbang.site.challenge.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sarangbang.site.challengecategory.entity.ChallengeCategory;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "Challenges")
 @NoArgsConstructor
+@Getter
+@Setter
 public class Challenge {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String location;
-    private String topic;
     private String title;
     private String descirption;
     private int participants;
@@ -23,4 +27,9 @@ public class Challenge {
     private LocalDate endDate;
     private String image;
     private char status;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private ChallengeCategory challengeCategory;
+
 }
