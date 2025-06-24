eefa7034a4aa7979158f166efe67f9e24819e8bc
SeongriPark <tjdfl0516@gmail.com>
2025-06-24 15:05:39 +0900

    챌린지 엔티티, 레포 생성 (#24)
    * 엔티티, 레포 생성
    
    * 엔티티, 레포 등록

---
+++ src/main/java/sarangbang/site/challenge/repository/ChallengeRepository.java
@@ -0,0 +1,10 @@
+package sarangbang.site.challenge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sarangbang.site.challenge.entity.Challenge;
+
+@Repository
+public interface ChallengeRepository extends JpaRepository<Challenge, Integer> {
+
+}
