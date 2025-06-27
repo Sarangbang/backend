fb1b7553fe631103a6ac876091fe9a0c8690e105
SeongriPark <tjdfl0516@gmail.com>
2025-06-27 18:23:21 +0900

    chore:
    - ChallengeApplicationService 챌린지 신청,거부 로직 수정

--- src/main/java/sarangbang/site/challengeapplication/service/ChallengeApplicationService.java
+++ src/main/java/sarangbang/site/challengeapplication/service/ChallengeApplicationService.java
@@ -16,7 +16,6 @@ public class ChallengeApplicationService {
 
     private final ChallengeApplicationRepository challengeApplicationRepository;
     private final ChallengeMemberService challengeMemberService;
-    private final ChallengeMemberRepository challengeMemberRepository;
 
     // 챌린지 신청 수락/거부
     public ChangeChallengeAppDTO changeApplicationStatus(Long appId, ChangeChallengeAppDTO dto, String ownerId) {
@@ -25,7 +24,7 @@ public class ChallengeApplicationService {
         if(app==null){
             throw new EntityNotFoundException("챌린지 "+appId+"를 찾을 수 없습니다.");
         }
-        ChallengeMember member = challengeMemberRepository.findChallengeMemberByUser_IdAndChallenge_Id(ownerId, app.getChallenge().getId()); // 특정 챌린지의 id 에서 member 조회
+        ChallengeMember member = challengeMemberService.getMemberByChallengeId(ownerId, app.getChallenge().getId()); // 특정 챌린지의 id 에서 member 조회
         if(member==null){
             throw new EntityNotFoundException("챌린지 멤버 "+member+"를 찾을 수 없습니다.");
         }
@@ -42,7 +41,11 @@ public class ChallengeApplicationService {
             } else {
                 app.updateAppStatus("approved");
                 app.updateAppComment(dto.getComment());
-                challengeMemberService.saveChallengeMember(app.getUser().getId(), appId);
+                ChallengeMember findMember = challengeMemberService.getMemberByChallengeId(app.getUser().getId(), app.getChallenge().getId());
+                if(findMember!=null){
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                }
+                challengeMemberService.saveChallengeMember(app.getUser().getId(), app.getChallenge().getId());
             }
             challengeApplicationRepository.save(app);
 
--- src/main/java/sarangbang/site/challengemember/service/ChallengeMemberService.java
+++ src/main/java/sarangbang/site/challengemember/service/ChallengeMemberService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import sarangbang.site.challenge.entity.Challenge;
 import sarangbang.site.challenge.repository.ChallengeRepository;
+import sarangbang.site.challenge.service.ChallengeService;
 import sarangbang.site.challengemember.entity.ChallengeMember;
 import sarangbang.site.challengemember.repository.ChallengeMemberRepository;
 import sarangbang.site.user.entity.User;
@@ -18,6 +19,7 @@ public class ChallengeMemberService {
     private final ChallengeMemberRepository challengeMemberRepository;
     private final ChallengeRepository challengeRepository;
     private final UserService userService;
+    private final ChallengeService challengeService;
 
     // 챌린지 오너 저장
     public void saveChallengeOwner(String userId, Long challengeId) {
@@ -38,11 +40,16 @@ public class ChallengeMemberService {
         User user = userService.getUserById(userId);
         log.debug("챌린지 등록 멤버 Id : {}", user.getId());
 
-        Challenge challenge = challengeRepository.findChallengeById(challengeId);
+        Challenge challenge = challengeService.getChallengeById(challengeId);
         log.debug("챌린지 등록 챌린지 Id : {}", challenge.getId());
 
         ChallengeMember challengeMember = new ChallengeMember("member", challenge, user);
 
         challengeMemberRepository.save(challengeMember);
+    }
+
+    // 특정 챌린지 id 의 member 조회
+    public ChallengeMember getMemberByChallengeId(String userId, Long challengeId) {
+        return challengeMemberRepository.findChallengeMemberByUser_IdAndChallenge_Id(userId, challengeId);
     }
 }
