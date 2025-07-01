05371e1492c0197df035618bcda2b90142fe0c31
SeongriPark <tjdfl0516@gmail.com>
2025-07-01 14:31:33 +0900

    챌린지 신청서 수락/거부 (#42)
    * feat: 챌린지신청서 도메인 생성
    
    * feat: 챌린지 신청서 수락/거부
    
    * feat: ChallengeApplication 엔티티 setter 삭제 후 메서드 생성
    
    * chore: ChallengeApplicationService 승인/거부 예외처리 강화
    
    * chore:
    
    - ChallengeApplicationService 승인/거부 예외처리 강화
    - ChangeChallengeAppDTO validation수정
    
    * chore:
    
    - ChallengeApplicationService 챌린지 신청,거부 로직 수정
    
    * chore:
    - ChallengeMemberService 이미 존재하는 멤버 확인 메서드 생성
    
    * chore:
    - ChallengeMemberService 수정
    
    * chore:
    - ChallengeMemberService 수정
    
    * chore:
    - swagger 추가
    
    * chore: ChallengeApplicationController 예외처리 추가

--- src/main/java/sarangbang/site/challengeapplication/service/ChallengeApplicationService.java
+++ src/main/java/sarangbang/site/challengeapplication/service/ChallengeApplicationService.java
@@ -3,13 +3,19 @@ package sarangbang.site.challengeapplication.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sarangbang.site.challengeapplication.dto.ChangeChallengeAppDTO;
+import sarangbang.site.challengeapplication.entity.ChallengeApplication;
 import sarangbang.site.challenge.entity.Challenge;
 import sarangbang.site.challenge.service.ChallengeService;
 import sarangbang.site.challengeapplication.dto.ChallengeJoinDTO;
-import sarangbang.site.challengeapplication.entity.ChallengeApplication;
 import sarangbang.site.challengeapplication.enums.ChallengeApplyStatus;
 import sarangbang.site.challengeapplication.exception.DuplicateApplicationException;
 import sarangbang.site.challengeapplication.repository.ChallengeApplicationRepository;
+import sarangbang.site.challengemember.entity.ChallengeMember;
+import sarangbang.site.challengemember.service.ChallengeMemberService;
+
+import java.util.Optional;
 import sarangbang.site.user.entity.User;
 import sarangbang.site.user.service.UserService;
 
@@ -19,8 +25,50 @@ import sarangbang.site.user.service.UserService;
 public class ChallengeApplicationService {
 
     private final ChallengeApplicationRepository challengeApplicationRepository;
+    private final ChallengeMemberService challengeMemberService;
     private final UserService userService;
     private final ChallengeService challengeService;
+
+    // 챌린지 신청 수락/거부
+    @Transactional
+    public ChangeChallengeAppDTO changeApplicationStatus(Long appId, ChangeChallengeAppDTO dto, String ownerId) {
+
+        ChallengeApplication app = challengeApplicationRepository.findChallengeApplicationById(appId);
+        if(app==null){
+            throw new IllegalArgumentException("챌린지 "+appId+"를 찾을 수 없습니다.");
+        }
+        Optional<ChallengeMember> member = challengeMemberService.getMemberByChallengeId(ownerId, app.getChallenge().getId()); // 특정 챌린지의 id 에서 member 조회
+        if(member.isEmpty()){
+            throw new IllegalArgumentException("챌린지 멤버 "+member+"를 찾을 수 없습니다.");
+        }
+
+        if(!app.getChallengeApplyStatus().equals(ChallengeApplyStatus.PENDING)){
+            throw new IllegalStateException("이미 처리된 신청입니다.");
+        }
+
+        if(member.get().getRole().equals("owner")) {
+
+            if(dto.getApplyStatus().equals(ChallengeApplyStatus.REJECTED)) {
+                app.updateAppStatus(ChallengeApplyStatus.REJECTED);
+                app.updateAppComment(dto.getComment());
+            } else {
+                app.updateAppStatus(ChallengeApplyStatus.APPROVED);
+                app.updateAppComment(dto.getComment());
+                Optional<ChallengeMember> findMember = challengeMemberService.getMemberByChallengeId(app.getUser().getId(), app.getChallenge().getId());
+                if(findMember.isPresent()){
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                }
+                challengeMemberService.saveChallengeMember(app.getUser().getId(), app.getChallenge().getId());
+            }
+            challengeApplicationRepository.save(app);
+
+            ChangeChallengeAppDTO changeApp = new ChangeChallengeAppDTO(app.getChallengeApplyStatus(), app.getComment());
+            return changeApp;
+
+        } else throw new SecurityException("챌린지 방장의 권한이 없습니다.");
+
+    }
+
 
     public ChallengeJoinDTO saveChallengeApplication(ChallengeJoinDTO challengeJoinDTO, String userId) {
         log.info("=> 챌린지 신청서 저장 로직 시작. userId: {}, challengeId: {}", userId, challengeJoinDTO.getChallengeId());
@@ -47,8 +95,8 @@ public class ChallengeApplicationService {
         );
         log.debug("... 신청서 엔티티 생성 완료. 상태를 PENDING으로 설정.");
 
-            log.info("... 챌린지 신청서 DB 저장 완료. applicationId: {}", challengeApplication.getId());
-            challengeApplicationRepository.save(challengeApplication);
+        log.info("... 챌린지 신청서 DB 저장 완료. applicationId: {}", challengeApplication.getId());
+        challengeApplicationRepository.save(challengeApplication);
 
         ChallengeJoinDTO responseDTO = new ChallengeJoinDTO(
                 challengeApplication.getIntroduction(),
