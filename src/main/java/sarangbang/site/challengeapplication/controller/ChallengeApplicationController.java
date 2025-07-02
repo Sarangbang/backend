6900aa6758d8cf217218edd94bdd1f201eeb7788
SeongriPark <tjdfl0516@gmail.com>
2025-07-02 13:40:30 +0900

    챌린지 등록 swagger 추가  (#62)
    - 챌린지 생성 swagger  추가
    - ChallengeController 챌린지 수락/거부 swagger 수정

--- src/main/java/sarangbang/site/challengeapplication/controller/ChallengeApplicationController.java
+++ src/main/java/sarangbang/site/challengeapplication/controller/ChallengeApplicationController.java
@@ -28,8 +28,7 @@ import sarangbang.site.challengeapplication.exception.ChallengeApplicationExcept
 import sarangbang.site.challengeapplication.service.ChallengeApplicationService;
 import sarangbang.site.security.details.CustomUserDetails;
 
-@Tag(name = "Challenge Application", description = "챌린지 신청 API")
-
+@Tag(name = "Challenge-application", description = "챌린지 신청서 관련 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -41,11 +40,11 @@ public class ChallengeApplicationController {
     // 챌린지 신청 수락/거부
     @Operation(summary = "챌린지 신청 수락/거부", description = "방장이 챌린지 신청서를 확인하고 참가 여부를 결정합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "정상적으로 처리됨"),
-            @ApiResponse(responseCode = "400", description = "입력값 오류 또는 잘못된 요청"),
-            @ApiResponse(responseCode = "403", description = "챌린지 방장이 아님"),
-            @ApiResponse(responseCode = "409", description = "이미 처리된 신청서거나 존재하는 멤버인 경우"),
-            @ApiResponse(responseCode = "500", description = "예기치 못한 오류 발생")
+            @ApiResponse(responseCode = "200", description = "정상적으로 처리됨", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChangeChallengeAppDTO.class))),
+            @ApiResponse(responseCode = "400", description = "입력값 오류 또는 잘못된 요청", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "챌린지 방장이 아님", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "409", description = "이미 처리된 신청서거나 존재하는 멤버인 경우", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(mediaType = "application/json"))
     })
     @PostMapping("/{appId}")
     public ResponseEntity<ChangeChallengeAppDTO> changeApplicationStatus(
