f5862941cdc7b84358f9769b9f431104fd2f8214
apfp77 <apfp77@naver.com>
2025-07-03 20:52:18 +0900

    feat: 로그인 실패 시 발생하는 exception handler 추가

--- src/main/java/sarangbang/site/user/exception/UserExceptionHandler.java
+++ src/main/java/sarangbang/site/user/exception/UserExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -39,6 +40,14 @@ public class UserExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(BadCredentialsException ex) {
+        log.warn("[User Domain Exception] BadCredentialsException: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse("잘못된 자격 증명입니다. 다시 시도해주세요."));
     }
 
     // user 패키지 내에서 처리되지 않은 나머지 예외들
