26847950af885db4eee80ff44495f655a7aed3a5
kimyoungjun-dev <kdaon1130@gmail.com>
2025-07-09 21:08:25 +0900

    feat: ChallengeResponseDto 기간 제거, 종료일 추가

--- src/main/java/sarangbang/site/challenge/dto/ChallengeResponseDto.java
+++ src/main/java/sarangbang/site/challenge/dto/ChallengeResponseDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import sarangbang.site.challenge.entity.Challenge;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 @Getter
 @NoArgsConstructor
@@ -20,7 +19,7 @@ public class ChallengeResponseDto {
     private int participants;
     private int currentParticipants;
     private LocalDate startDate;
-    private Long period;
+    private LocalDate endDate;
 
     public ChallengeResponseDto(Challenge challenge, int currentParticipants) {
         this.id = challenge.getId();
@@ -30,12 +29,7 @@ public class ChallengeResponseDto {
         this.participants = challenge.getParticipants();
         this.currentParticipants = currentParticipants;
         this.startDate = challenge.getStartDate();
+        this.endDate = challenge.getEndDate();
 
-        // 시작일과 종료일이 모두 있을 경우 기간(일) 계산
-        if (challenge.getStartDate() != null && challenge.getEndDate() != null) {
-            this.period = ChronoUnit.DAYS.between(challenge.getStartDate(), challenge.getEndDate());
-        } else {
-            this.period = 0L; // 날짜 정보가 없을 경우 기본값 0으로 설정
-        }
     }
 }
