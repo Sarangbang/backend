2fa510e0cee4cd596d289c83e5d0ac7d5b7e3670
apfp77 <apfp77@naver.com>
2025-07-20 20:19:57 +0900

    feat: 챌린지 이미지 추가

---
+++ src/main/java/sarangbang/site/file/service/ImageSaveFactory.java
@@ -0,0 +1,23 @@
+package sarangbang.site.file.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import sarangbang.site.file.enums.ImageType;
+
+@Service
+@RequiredArgsConstructor
+public class ImageSaveFactory {
+
+    private final ImageUploadService imageUploadService;
+
+    public String getImageUploadService(MultipartFile file, ImageType type, Long personId) {
+        imageUploadService.validateImageFile(file);
+        return switch (type) {
+            case PROFILE -> imageUploadService.storeProfileImage(file, personId);
+            case CHALLENGE -> imageUploadService.storeChallengeImage(file, personId);
+            case VERTIFICATION -> imageUploadService.storeProofImage(file, personId);
+            default -> throw new IllegalArgumentException("지원하지 않는 이미지 타입입니다: " + type);
+        };
+    }
+}
