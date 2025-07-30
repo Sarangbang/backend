d823cb2d1f1af560a94a970ed4584394f8f13933
kimyoungjun-dev <kdaon1130@gmail.com>
2025-07-30 10:52:32 +0900

    기기별 리프레시 토근 관리 로직 변경 (#153)
    * feat: InvalidRefreshTokenException 추가, InvalidRefreshTokenException추가로 인한 코드 변경
    
    * feat: 기기별 리프레시 토큰 괸리 구현.
    RefreshToken 엔티티 필드 변경, 레포지토리 코드 변경, 서비스 코드 변경
    
    * feat: 기기별 리프레시 토큰 괸리 구현.
    CustomUserDetails 코드 변경, AuthService 코드 변경, AuthController 코드 변경
    
    * feat: 기기별 리프레시 토큰 관리 구현으로 인한 refresh token 발급 코드 수정
    
    * feat: device_info 필드 user-agent를 UUID로 변경으로 인한 코드 수정

--- src/main/java/sarangbang/site/security/details/CustomUserDetails.java
+++ src/main/java/sarangbang/site/security/details/CustomUserDetails.java
@@ -1,7 +1,6 @@
 package sarangbang.site.security.details;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,45 +10,48 @@ import java.util.Collection;
 import java.util.Collections;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
-    private final String id;  // UUID
-    private final String email;
-    private final String nickname;
-    private final String password;
-    private final String profileImageUrl;
+    private final User user;
     private final Collection<? extends GrantedAuthority> authorities;
 
     public CustomUserDetails(User user, Collection<? extends GrantedAuthority> authorities) {
-        this.id = user.getId();
-        this.email = user.getEmail();
-        this.nickname = user.getNickname();
-        this.password = user.getPassword();
-        this.profileImageUrl = user.getProfileImageUrl();
+        this.user = user;
         this.authorities = authorities;
     }
 
     public CustomUserDetails(User user) {
-        this.id = user.getId();
-        this.email = user.getEmail();
-        this.nickname = user.getNickname();
-        this.password = user.getPassword();
-        this.profileImageUrl = user.getProfileImageUrl();
+        this.user = user;
         this.authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
     public String getUsername() {
-        return email;
+        return user.getEmail();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return user.getPassword();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
+    }
+
+    public String getId() {
+        return user.getId();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+
+    public String getNickname() {
+        return user.getNickname();
+    }
+
+    public String getProfileImageUrl() {
+        return user.getProfileImageUrl();
     }
 }
