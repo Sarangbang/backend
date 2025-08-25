403f286ee8abe0709a9891e4912c576b28be3b7a
apfp77 <apfp77@naver.com>
2025-08-25 15:15:29 +0900

    fix: ssm timeout, challenge not found exception

--- src/main/java/sarangbang/site/global/error/ErrorCode.java
+++ src/main/java/sarangbang/site/global/error/ErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
     MISSING_REQUEST_PARAMETER(400, "필수 요청 파라미터가 누락되었습니다."),
-    DIARY_NOT_FOUND(404, "해당 일기 목록이 존재하지 않습니다."),
+    CHALLENGE_NOT_FOUND(404, "해당 챌린지를 찾을 수 없습니다."),
 
     INTERNAL_SERVER_ERROR(500, "서버에 오류가 발생했습니다."),
 
